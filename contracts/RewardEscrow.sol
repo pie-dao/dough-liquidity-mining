e5e7052c4ef46c63a3ee9b65d8b462f02aa692a6
Mick de Graaf <mick@mickdegraaf.nl>
2020-10-19 17:14:23 +0200

    Combine vesting shedules in the same day

--- contracts/RewardEscrow.sol
+++ contracts/RewardEscrow.sol
@@ -460,7 +460,16 @@ contract RewardEscrow is Owned {
             totalEscrowedAccountBalance[account] = totalEscrowedAccountBalance[account].add(quantity);
         }
 
-        vestingSchedules[account].push([time, quantity]);
+        // If last window is less than a day old add amount to that one.
+        if(
+            vestingSchedules[account].length != 0 && 
+            vestingSchedules[account][vestingSchedules[account].length - 1][0] > now - 1 days
+        ) {
+            vestingSchedules[account][vestingSchedules[account].length - 1][1] = vestingSchedules[account][vestingSchedules[account].length - 1][1].add(quantity);
+        } else {
+            vestingSchedules[account].push([time, quantity]);
+        }
+        
 
         emit VestingEntryCreated(account, now, quantity);
     }
